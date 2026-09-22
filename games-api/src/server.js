15d7118bf7984b0cf14d1bf0530dfdea28837151
Lukáš <kyselalukas@seznam.cz>
2026-09-22 14:14:01 +0200

    Allow paged monthly IGDB catalog refresh

--- games-api/src/server.js
+++ games-api/src/server.js
@@ -91,7 +91,7 @@ app.use('/api/enrich', expensiveRateLimit(30));
 app.use('/api/search', expensiveRateLimit(60));
 app.use('/api/discover', expensiveRateLimit(45));
 app.use('/api/igdb/catalog-batch', expensiveRateLimit(12));
-app.use('/api/igdb/catalog-range', expensiveRateLimit(30));
+app.use('/api/igdb/catalog-range', expensiveRateLimit(90));
 
 const asyncRoute = handler => async (req, res, next) => {
   try { await handler(req, res, next); }
